75bce4e7c7ba73b3054796282fd681ed775f3924
Kiyotaka07 <wizarddark856@gmail.com>
2025-05-03 19:22:19 +0545

    Update kanda.js

--- scripts/cmds/kanda.js
+++ scripts/cmds/kanda.js
@@ -1,59 +1,83 @@
-module.exports = {
-  config: {
-    name: "kanda",
-    aliases: ["kanda1"],
-    version: "2.0",
-    author: "Alwaysone",
-    countDown: 10,
-    role: 2,
-    shortDescription: "",
-    longDescription: "Fetch kanda videos",
-    category: "18+",
-    guide: "{p}{n}",
-  },
-
-  sentVideos: [],
-
-  onStart: async function ({ api, event, message }) {
-    const senderID = event.senderID;
-
-    // Loading message
-    const loadingMessage = await message.reply({
-      body: "Aaudai xa ruk muji 🤡",
-    });
-
-    const videoLinks = [
-      "https://drive.google.com/uc?id=1-bMhZCbyD3thooI0-xSc1f9V64GwCU0S&export=download",
-      "https://drive.google.com/uc?id=1-Z_05IyEpcYcLDSBZP6DGDHoTjgdgWWr&export=download",
-      "https://drive.google.com/uc?id=1-JuVAaghvVju2ar4HoAUn9znkoVF2UND&export=download",
-      "https://drive.google.com/uc?id=1-6NkQ6ySuf_O966Xu3dB6goAuxW6RfrZ&export=download",
-      "https://drive.google.com/uc?id=1-3QMhPqgogMf-nRH2XUBwnBZmfEewSY_&export=download",
-      "https://drive.google.com/uc?id=1-UXCvIQPEmjcYKqrOn1U5Iy5_nC-zaBG&export=download",
-      "https://drive.google.com/uc?id=1-IMsgc93miME2rmFcF8j3IX051POOopL&export=download",
-      "https://drive.google.com/uc?id=1-JXYmh5YdO_VFFHdObvZ4AVHp-r69Yc1&export=download"
-    ];
-
-    try {
-      const randomVideo = videoLinks[Math.floor(Math.random() * videoLinks.length)];
-      
-      // Updated SUCCESS message below 👇
-      await message.reply({
-        body: "Xolerai jindagi jane vayo tero 🤡", // New attached message
-        attachment: await global.utils.getStreamFromURL(randomVideo)
-      });
-    } catch (error) {
-      console.error("Error sending video:", error);
-      // Updated ERROR message below 👇
-      await message.reply({
-        body: "Tero vagya xaina muji feri try gar 🤡", // Changed error message
-      });
+[
+    {
+        "key": "sb",
+        "value": "ec9IZmuU75LeSiFzzD9FzF2T",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-05-03T13:37:11.160Z",
+        "lastAccessed": "2025-05-03T13:37:11.160Z"
+    },
+    {
+        "key": "m_ls",
+        "value": "%7B%2261561844279122%22%3A%7B%22c%22%3A%7B%221%22%3A%22HCwAABaMAxbItJuAARMFFqSd4b2v_xsA%22%2C%222%22%3A%22GRwVQBxMAAAWARaQt-XpDBYAFpC35ekMABYoAA%22%2C%2295%22%3A%22HCwAABYMFv7ustMEEwUWpJ3hva__GwA%22%7D%2C%22d%22%3A%22290970c5-a83e-46a9-8cb5-3ae67a61d3ac%22%2C%22s%22%3A%220%22%2C%22u%22%3A%22epg5sj%22%7D%2C%2261564185899750%22%3A%7B%22c%22%3A%7B%221%22%3A%22HCwAABaAMhaWtpLSBRMFFszL8_bA_xsA%22%2C%222%22%3A%22GRwVQBxMAAAWBBaMq_vrDBYAFoyr--sMABYoAA%22%2C%2295%22%3A%22HCwAABYKFvzD_5wHEwUWzMvz9sD_GwA%22%7D%2C%22d%22%3A%22fd059ae5-edd1-48f0-8815-afa41e49e6ef%22%2C%22s%22%3A%220%22%2C%22u%22%3A%22xb7qr0%22%7D%7D",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-05-03T13:37:11.161Z",
+        "lastAccessed": "2025-05-03T13:37:11.161Z"
+    },
+    {
+        "key": "dpr",
+        "value": "1.375",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-05-03T13:37:11.161Z",
+        "lastAccessed": "2025-05-03T13:37:11.161Z"
+    },
+    {
+        "key": "datr",
+        "value": "JyITaJ82fycRJe5Rm5rd96RD",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-05-03T13:37:11.161Z",
+        "lastAccessed": "2025-05-03T13:37:11.161Z"
+    },
+    {
+        "key": "c_user",
+        "value": "100069325455607",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-05-03T13:37:11.161Z",
+        "lastAccessed": "2025-05-03T13:37:11.161Z"
+    },
+    {
+        "key": "xs",
+        "value": "1%3ApyQnKhTL6H9LwA%3A2%3A1746279088%3A-1%3A3594",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-05-03T13:37:11.161Z",
+        "lastAccessed": "2025-05-03T13:37:11.161Z"
+    },
+    {
+        "key": "fr",
+        "value": "06gTKA1FZcmTJF3WD.AWdFA9sG--arIOXYJhu_tUjkKJIGgiPCrdEu5LpHZRc1RYqtcsU.BoEyIn..AAA.0.0.BoFhq0.AWdAu2iqI5GyvhBkysH7aSG5LNk",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-05-03T13:37:11.161Z",
+        "lastAccessed": "2025-05-03T13:37:11.161Z"
+    },
+    {
+        "key": "wd",
+        "value": "1359x617",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-05-03T13:37:11.161Z",
+        "lastAccessed": "2025-05-03T13:37:11.161Z"
+    },
+    {
+        "key": "presence",
+        "value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1746279352605%2C%22v%22%3A1%7D",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-05-03T13:37:11.161Z",
+        "lastAccessed": "2025-05-03T13:37:11.161Z"
     }
-
-    // Cleanup loading message
-    try {
-      await loadingMessage.delete();
-    } catch (error) {
-      console.error("Error deleting message:", error);
-    }
-  },
-};
+]
